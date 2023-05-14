08a6fc7970975d9b28cb1b6665a59a94a24397bb
adiomi90 <adiomi90@outlook.com>
2023-05-14 12:17:32 +0300

    modules

---
+++ module/app.py
@@ -0,0 +1,23 @@
+from ecommerce.shopping import sales
+
+
+
+
+
+
+
+
+
+# from ecommerce.shopping import sales
+# from ecommerce.customer import contact
+# import sys
+
+
+
+# sales.contact
+# contact.contact()
+# print(sys.path)
+
+
+
+
